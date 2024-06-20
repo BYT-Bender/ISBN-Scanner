dfdaa549d1441ebbcbf11b37366fb5d7dc967ff0
makardvaj <141413540+makardvaj@users.noreply.github.com>
2024-06-20 20:48:37 +0530

    Update ISBN2Details.py

--- ISBN2Details.py
+++ ISBN2Details.py
@@ -9,10 +9,25 @@ def ISBN2Details(ISBN):
             return None
         else:
             title = BookData["items"][0]["volumeInfo"]["title"]
-            author = BookData["items"][0]["volumeInfo"]["authors"][0]
+            authors = BookData["items"][0]["volumeInfo"]["authors"]
             publisher = BookData["items"][0]["volumeInfo"]["publisher"]
-            publish_date = BookData["items"][0]["volumeInfo"]["publishedDate"]
-            book_details = {"title": title, "author": author, "publisher": publisher, "publish_date": publish_date}
+            publishedDate = BookData["items"][0]["volumeInfo"]["publishedDate"]
+            description = BookData["items"][0]["volumeInfo"]["description"]
+            pageCount = BookData["items"][0]["volumeInfo"]["pageCount"]
+            categories = BookData["items"][0]["volumeInfo"]["categories"]
+            language = BookData["items"][0]["volumeInfo"]["language"]
+            
+            book_details = {
+                "ISBN-13" : ISBN,
+                "Title" : title,
+                "Author" : authors[0],
+                "Publisher" : publisher,
+                "Edition": publishedDate,
+                "Description" : description,
+                "Pages" : pageCount,
+                "Genre" : categories,
+                "Language" : language
+            }
             return book_details
     else:
         return None
