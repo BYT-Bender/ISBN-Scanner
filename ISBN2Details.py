5051bf7414c6301dbe0e8e6e746cb8373eaf4de2
makardvaj <141413540+makardvaj@users.noreply.github.com>
2024-06-20 18:43:52 +0530

    Update ISBN2Details.py

--- ISBN2Details.py
+++ ISBN2Details.py
@@ -1,1 +1,16 @@
+import requests
 
+def ISBN2Details(ISBN):
+    URL = "https://www.googleapis/com/books/v1/volumes?q=isbn:" + ISBN
+    Response = requests.get(url)
+    if Response.status_code == 200:
+        BookData = Response.json()
+        if BookData["totalItems"] == 0:
+            return None
+        else :
+            Title = BookData["items"][0]["volumeInfo"]["title"]
+            Author = BookData["items"][0]["volumeInfo"]["title"][0]    #First author is the main one.
+            Publisher = BookData["items"][0]["volumeInfo"]["publisher"]
+            Publish_Date = BookData["items"][0]["volumeInfo"]["publishedDate"]
+            Details_Dict = {"Title" : Title, "Author" : Author, "Publisher" : Publisher, "Publish_Date" : Publish_Date}
+            return Details_Dict
