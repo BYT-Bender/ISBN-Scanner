4d249fef5bb9d23a5ef07f4f1a7a68ed21ba00ce
Byte Bender <136265142+BYT-Bender@users.noreply.github.com>
2024-06-20 18:58:51 +0530

    Update ISBN2Details.py

--- ISBN2Details.py
+++ ISBN2Details.py
@@ -1,18 +1,18 @@
 import requests
 
 def ISBN2Details(ISBN):
-    URL = "https://www.googleapis/com/books/v1/volumes?q=isbn:" + str(ISBN)
+    URL = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + str(ISBN)
     Response = requests.get(URL)
     if Response.status_code == 200:
         BookData = Response.json()
         if BookData["totalItems"] == 0:
             return None
-        else :
+        else:
             Title = BookData["items"][0]["volumeInfo"]["title"]
-            Author = BookData["items"][0]["volumeInfo"]["title"][0]    #First author is the main one.
+            Author = BookData["items"][0]["volumeInfo"]["authors"][0]
             Publisher = BookData["items"][0]["volumeInfo"]["publisher"]
             Publish_Date = BookData["items"][0]["volumeInfo"]["publishedDate"]
-            Details_Dict = {"Title" : Title, "Author" : Author, "Publisher" : Publisher, "Publish_Date" : Publish_Date}
+            Details_Dict = {"Title": Title, "Author": Author, "Publisher": Publisher, "Publish_Date": Publish_Date}
             return Details_Dict
     else:
         return None
