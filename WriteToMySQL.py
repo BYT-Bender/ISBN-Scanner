ce93b6a727b4c8ae77850268ce7d379196329efb
makardvaj <141413540+makardvaj@users.noreply.github.com>
2024-06-21 11:07:52 +0530

    Update WriteToMySQL.py
    Added condition to check for the presence of headers.

--- WriteToMySQL.py
+++ WriteToMySQL.py
@@ -1,12 +1,19 @@
-from ISBN2Details import ISBN2Details
 import csv
+import os
 
-File = "BookCollection.csv"
-fields = ["ISBN-13", "Title", "Author", "Publisher", "Edition", "Description", "Pages", "Genre", "Language"]
-My_Dict = []
-# galat hai
-
-with open(File, 'w') as CSVFile :
-  MyWriter = csv.writer(CSVFile, fieldnames = fields)
-  MyWriter.writeheader()
-  MyWriter.writerows(My_Dict)
+def PushToCSV(ISBN_Dict)
+    File = "BookCollection.csv"
+    fields = ["ISBN-13", "Title", "Author", "Publisher", "Edition", "Description", "Pages", "Genre", "Language"]
+    My_Dict = ISBN_Dict
+    
+    with open(File, 'w') as CSVFile :
+        MyWriter = csv.writer(CSVFile, fieldnames = fields)
+        
+        if not(os.path.exists(File)):    # Headers WILL necessarily be present if the .csv file already exists.
+            MyWriter.writeheader()
+        else :
+            continue
+        
+        MyWriter.writerows(My_Dict)
+    
+    CSVFile.close()
